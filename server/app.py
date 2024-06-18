954072074c930f248a2a01ed84cec3fbb12a687d
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-06-18 17:49:08 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -43,10 +43,40 @@ api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
 
-    def get(self, id):
+    def get(self,id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
 
+        plant = Plant.query.filter_by(id = id).first()
+        
+        plant.is_in_stock = request.get_json().get('is_in_stock')
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(
+            plant_dict,
+            200
+        )
+        return  response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_body = {}
+
+        response = make_response(
+            response_body,
+            204
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
